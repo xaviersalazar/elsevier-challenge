424a5288771d4e0f130b0b18ba88b50e9e2f0513
Xavier Salazar <xavierssalazar92@gmail.com>
2022-11-17 14:39:10 -0600

    adding tailwind configs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 function App() {
-  return <h1>Elselvier Challenge</h1>;
+  return <h1 className="text-5xl font-bold">Elselvier Challenge</h1>;
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontWeight: {
+      thin: 100,
+      regular: 400,
+      medium: 500,
+      bold: 700,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
